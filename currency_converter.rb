17fd88e6a03f290de026463c5ba227ffc8afa335
Danai Adkisson <dadkisso@live.com>
2015-05-08 19:04:52 -0400

    finished normal mode, no README

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,20 +1,21 @@
+class  CurrencyDenominationError < StandardError
+end
+
 class CurrencyConverter
-  attr_reader :conversion_rates
+  attr_reader :conversion_rates, :currency_object, :code
 
   def initialize(conversion_rates)
     @conversion_rates = conversion_rates
   end
 
-  def convert(currency_object, code)
-    if code == currency_object.code
+  def convert (currency_object, code)
+    if  !conversion_rates.include?(code) || !conversion_rates.include?(currency_object.code)
+       raise CurrencyDenominationError, "Currency Denomination Not Found!"
+    elsif code == currency_object.code
       Currency.new(code, currency_object.amount)
     else
-      Currency.new(code, currency_object.amount * conversion_rates[code])
+      Currency.new(code, currency_object.amount * conversion_rates[code] / conversion_rates[currency_object.code])
     end
-  end
-
-  def god_help_me
-    Currency.new()
   end
 
 
--- currency_main.rb
+++ currency_main.rb
@@ -2,18 +2,20 @@ require "./currency_converter.rb"
 require "./currency_ammount.rb"
 
 usd = Currency.new(:USD, 1)
-usd_happy = Currency.new(:USD, 2.4)
-eur = Currency.new(:EUR, 0.74)
-
-currency_values = {USD: 1.0, EUR: 0.74, ILS:1,}
+usd_happy = Currency.new(:USD, 1)
+eur = Currency.new(:EUR, 1)
+ils = Currency.new(:ILS, 1)
+whatever = Currency.new(:BBB, 1)
+currency_values = {USD: 1.0, EUR: 0.74, ILS: 3.87}
 
 converter = CurrencyConverter.new(currency_values)
 
 p usd.code
 
 p converter.convert(usd, :USD) == usd
+
 #the below puts coresponds with line 12 in CurrencyConverter class
-p converter.convert(usd, :USD)
+p converter.convert(whatever, :BBB)
 
 
 #p something.something_else()
