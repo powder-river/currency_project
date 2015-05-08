31d0f4c6374b452b5df437530cb5d3029bebebdd
Danai Adkisson <dadkisso@live.com>
2015-05-08 13:26:43 -0400

    created methods in currency class  add subtract and multiply and also check for equality in currency code

--- currency_ammount.rb
+++ currency_ammount.rb
@@ -1,36 +1,51 @@
+class  CurrencyDenominationError < StandardError
+end
 
 class Currency
-  attr_reader :name, :amount
 
-  def initialize (name, amount)
-    @name = name
+  attr_reader :code, :amount
+
+  def initialize (code, amount)
+    @code = code
     @amount = amount
   end
 
-#lines 11-17 sets both @amount to equal true. do not yet know why this is important
-  def equals
-  @amount == @amount
-  @name == @name
-  end
-
-  def == (other)
-    equals == other
+#lines 11-14 checks that currency objects with same
+# currncy ammount and codes are equal
+  def ==(currency)
+    code == currency.code &&
+    amount == currency.amount
   end
 
 #lines 20-28 establishes the !equals but again not sure if I did this correct
-  def not_equal
-    @ammount != @amount
-    @name != @name
+  def + (currency)
+    if code == currency.code
+
+      Currency.new(code, amount + currency.amount)
+    else
+      raise CurrencyDenominationError
+        puts "Currency must match to add"
+    end
+
+
   end
 
-  def != (other)
-    not_equal != other
+  def - (currency)
+    if code == currency.code
+       Currency.new(code, amount - currency.amount)
+
+    else
+      puts "screw you, not possible"
+    end
   end
 
-  def + (other)
-    Currency.new(@name + (@amount * other), @amount)
+  def * (currency)
+    if code == currency.code
+      Currency.new(code, amount * currency.amount)
+    else
+      "screw you, not possible"
+    end
   end
-
 
 
 
--- currency_main.rb
+++ currency_main.rb
@@ -1,25 +1,22 @@
-#require "./currency_converter.rb"
+require "./currency_converter.rb"
 require "./currency_ammount.rb"
 
 usd = Currency.new("USD", 1)
-usd_happy = Currency.new("USD", 2)
+usd_happy = Currency.new("USD", 2.4)
 eur = Currency.new("Eur", 0.75)
 
-equality = (usd),(eur)
+#equality = (usd),(eur)
 #us_conversian_rate = Converter.new([:USD])
 
-puts usd.name
-puts usd.amount
+#def i_must_have_value(value)
+#  raise TypeError, "you must give me truth" if value == false
+#end
+#
+#i_must_have_value(false)
+# p usd.name
+p usd + eur
 
-puts eur.name
-puts eur.amount
+# p usd != usd
 
-puts usd.equals
-puts eur.equals
-
-puts usd.not_equal
-puts eur.not_equal
-
-puts usd == eur
-
-puts usd.amount + usd_happy.amount
+# usd.equal(usd_happy)
+#puts usd == usd
