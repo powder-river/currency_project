af6799a64b3829b1ba1825a45402f2942ad9aa7b
Danai Adkisson <dadkisso@live.com>
2015-05-07 22:04:33 -0400

    added object for != and and addition object. still unclear if it is what instructions ask for.

--- currency_ammount.rb
+++ currency_ammount.rb
@@ -1,4 +1,4 @@
-#yes i know I this is a typo but I already created a git
+
 class Currency
   attr_reader :name, :amount
 
@@ -7,15 +7,31 @@ class Currency
     @amount = amount
   end
 
+#lines 11-17 sets both @amount to equal true. do not yet know why this is important
   def equals
-
   @amount == @amount
-
+  @name == @name
   end
 
   def == (other)
     equals == other
   end
+
+#lines 20-28 establishes the !equals but again not sure if I did this correct
+  def not_equal
+    @ammount != @amount
+    @name != @name
+  end
+
+  def != (other)
+    not_equal != other
+  end
+
+  def + (other)
+    Currency.new(@name + (@amount * other), @amount)
+  end
+
+
 
 
 end
--- currency_main.rb
+++ currency_main.rb
@@ -2,12 +2,24 @@
 require "./currency_ammount.rb"
 
 usd = Currency.new("USD", 1)
+usd_happy = Currency.new("USD", 2)
 eur = Currency.new("Eur", 0.75)
+
+equality = (usd),(eur)
 #us_conversian_rate = Converter.new([:USD])
 
 puts usd.name
 puts usd.amount
+
 puts eur.name
 puts eur.amount
 
 puts usd.equals
+puts eur.equals
+
+puts usd.not_equal
+puts eur.not_equal
+
+puts usd == eur
+
+puts usd.amount + usd_happy.amount
